a078c6a8092cb14b0ba77e81877a11ddfa5be15a
vishnu biju <vishnubiju.in@gmail.com>
2022-12-29 10:59:38 +0530

    setting up server and mongodb schema

---
+++ server/modules/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Object } = mongoose.Schema;
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+    index:true,
+  },
+  role:{
+    type: String,
+    default: 'subscriber',
+  },
+  cart: {
+    type:Array,
+    default: [],
+  },
+  address:String,
+ // wishlist:[{ type: ObjectId, ref: "Product" }],
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model('User',userSchema);
---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const {readdirSync} = require("fs");
+require("dotenv").config();
+
+
+
+//app
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: false,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(`DB CONNECTION ERROR: ${err}`));
+
+// middlewares
+  app.use(morgan("dev"));
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  //routes middleware
+      // to autoload routes
+  readdirSync("./routes").map((r) =>
+    app.use("/api", require("./routes/" + r))
+  );
+
+//port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`server is running on port ${port}`));

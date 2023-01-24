a2b28fa5ce6501503c228c3e83e3bd19687ce25b
vishnu biju <vishnubiju.in@gmail.com>
2023-01-24 12:03:46 +0530

    remove product backend completed

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,28 +1,38 @@
-const Product =  require('../models/product');
+const Product = require("../models/product");
 const slugify = require("slugify");
 
-
-exports.create =  async (req,res) => { 
- try {
-
+exports.create = async (req, res) => {
+  try {
     console.log(req.body);
     req.body.slug = slugify(req.body.title);
     const newProduct = await new Product(req.body).save();
     res.json(newProduct);
- } catch (err) {
+  } catch (err) {
     console.log(err);
-      res.status(400).json({
-         err: err.message,
-      })
+    res.status(400).json({
+      err: err.message,
+    });
   }
-};    
+};
 
 exports.listAll = async (req, res) => {
-   let product = await Product.find({})
-     .limit(parseInt(req.params.count))
-     .populate("category")
-     .populate("subs")
-     .sort([["createdAt", "desc"]])
-     .exec();
-   res.json(product);
- };
+  let product = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate("category")
+    .populate("subs")
+    .sort([["createdAt", "desc"]])
+    .exec();
+  res.json(product);
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Product.findOneAndRemove({
+      slug: req.params.slug,
+    }).exec();
+    res.json(deleted);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Product delete failed");
+  }
+};

e40deef31d029331b54cf49750901851bc11bab3
vishnu biju <vishnubiju.in@gmail.com>
2023-01-29 01:42:01 +0530

    home page view completed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,36 @@
-import React from 'react'; 
+import React from "react";
+import Jumbotron from "../components/cards/jumbotron";
+import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers"
+
 
 const Home = () => {
+
+
+
   return (
-    <div>
-      <p> Home page</p>
+    <div className="home-content">
+      <div className="jumbo">
+        <Jumbotron
+          text={["Latest Products", "New Arrivals", " Best Sellers"]}
+        />
+      </div>
+      <h4 className="jumbow">
+        New Arrivals
+      </h4>
+
+      <NewArrivals/>
+      <br/>
+      <br/>
+      <h4 className="jumbow">
+        Best Sellers
+      </h4>
+
+      <BestSellers/>
+      <br/>
+      <br/>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
-

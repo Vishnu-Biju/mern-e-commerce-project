d842da44855170e4f36a9aec903bbce332d461cc
vishnu biju <vishnubiju.in@gmail.com>
2023-02-03 16:29:20 +0530

    cart reducer added

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,15 +1,42 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Card, Tooltip } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.png";
 import { Link } from "react-router-dom";
 import { showAverage } from "../../functions/ratings";
+import _ from "lodash";
 
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+  const [tooltip, setTooltip] = useState("Click to add");
+
+  const handleAddToCart = () => {
+    // create cart array
+    let cart = [];
+    if (typeof window !== "undefined") {
+      // if cart is in local storage GET it
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      // push new product to cart
+      cart.push({
+        ...product,
+        count: 1,
+      });
+      // remove duplicates
+      //npm i lodash
+      let unique = _.uniqWith(cart, _.isEqual);
+      // save to local storage
+      console.log("unique", unique);
+      localStorage.setItem("cart", JSON.stringify(unique));
+       // show tooltip
+       setTooltip("Added");
+
+    }
+  };
   // destructure
-  const { images, title, description, slug,price } = product;
+  const { images, title, description, slug, price } = product;
   return (
     <div id="box">
       {product && product.ratings && product.ratings.length > 0 ? (
@@ -19,7 +46,7 @@ const ProductCard = ({ product }) => {
           NO RATINGS YET
         </div>
       )}
-      
+
       <Card
         cover={
           <img
@@ -32,9 +59,12 @@ const ProductCard = ({ product }) => {
           <Link to={`/product/${slug}`}>
             <EyeOutlined className="text-warning" /> <br /> View Product
           </Link>,
-          <>
-            <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-          </>,
+          <Tooltip title={tooltip}>
+            <a onClick={handleAddToCart}>
+              <ShoppingCartOutlined className="text-danger" /> <br /> Add to
+              Cart
+            </a>
+          </Tooltip>,
         ]}
       >
         <Meta

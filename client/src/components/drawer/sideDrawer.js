aa236eb53317e00fa1a1c740e8704a2b6e54c15e
vishnu biju <vishnubiju.in@gmail.com>
2023-02-04 19:10:07 +0530

    side drawer modified

--- client/src/components/drawer/sideDrawer.js
+++ client/src/components/drawer/sideDrawer.js
@@ -4,11 +4,69 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import laptop from "../../images/laptop.png";
 
-const SideDrawer = ({ children }) => {
+const SideDrawer = () => {
   const dispatch = useDispatch();
   const { drawer, cart } = useSelector((state) => ({ ...state }));
 
-  return <Drawer visible={true}>{JSON.stringify(cart)}</Drawer>;
+  const imageStyle = {
+    width: "60%",
+    height: "140px",
+    objectFit: "cover",
+  };
+
+  return (
+    <Drawer
+    style={{marginTop:"50px",paddingTop:"20px"}}
+      className="text-center"
+      title={`Cart / ${cart.length} Product`}
+      placement="right"
+      closable={false}
+      onClose={() => {
+        dispatch({
+          type: "SET_VISIBLE",
+          payload: false,
+        });
+      }}
+      open={drawer}
+    >
+      {cart.map((p) => (
+        <div key={p._id} className="row">
+          <div className="col ">
+            {p.images[0] ? (
+              <>
+                <img src={p.images[0].url} style={imageStyle} />
+                <p className="text-center bg-secondary text-light">
+                  {p.title} x {p.count}
+                </p>
+              </>
+            ) : (
+              <>
+                <img src={laptop} style={imageStyle} />
+                <p className="text-center bg-secondary text-light">
+                  {p.title} x {p.count}
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      ))}
+
+      <Link to="/cart">
+        <button
+          onClick={() =>
+            dispatch({
+              type: "SET_VISIBLE",
+              payload: false,
+            })
+          }
+          className="text-center btn btn-raised btn-block"
+          style={{backgroundColor:"#088178" , color:"#fff"}}
+        >
+          Go To Cart
+        </button>
+      </Link>
+    </Drawer>
+  );
 };
 
 export default SideDrawer;
